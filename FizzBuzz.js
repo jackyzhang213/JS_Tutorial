3dbddb284d7d88e586e7cf4877695ce282caca73
JackyZhang <jackyzhang213@gmail.com>
2013-12-23 18:38:03 +0800

    initial version

---
+++ FizzBuzz.js
@@ -0,0 +1,27 @@
+/**
+ * Problem: Write a program that prints the integers from 1 to 100.
+ *          But for multiples of three print "Fizz" instead of the
+ *          number and for the multiples of five print "Buzz".
+ *          For numbers which are multiples of both three and five print "FizzBuzz".
+ * User: jackyzhang
+ * Date: 13-12-23
+ */
+(function () {
+    var i,
+        n = 100;
+
+    for (i = 1; i <= n; i++) {
+        if (i % 15 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+
+    }
+
+
+}());
