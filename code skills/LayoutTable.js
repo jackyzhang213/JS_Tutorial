ba65b7f9d0639d7694e133d509698c4a9dc21690
Yu Zhang <jackyzhang213@gmail.com>
2014-01-13 09:56:10 +0800

    format the code with JSLint

--- code skills/LayoutTable.js
+++ code skills/LayoutTable.js
@@ -3,19 +3,19 @@
  *          laid out table—meaning that the columns are straight and the rows are aligned.
  *
  *          name           height country
-            -------------- ------ -------------
-            Kilimanjaro      5895 Tanzania
-            Everest          8848 Nepal
-            Mount Fuji       3776 Japan
-            Mont Blanc       4808 Italy/France
-            Vaalserberg       323 Netherlands
-            Mount McKinley   6168 United States
-            Popocatepetl     5465 Mexico
+ *          -------------- ------ -------------
+ *          Kilimanjaro      5895 Tanzania
+ *          Everest          8848 Nepal
+ *          Mount Fuji       3776 Japan
+ *          Mont Blanc       4808 Italy/France
+ *          Vaalserberg       323 Netherlands
+ *          Mount McKinley   6168 United States
+ *          Popocatepetl     5465 Mexico
  * User: JackyZhang213
  * Date: 14-1-10
  */
 
-(function(){
+(function () {
 
     var MOUNTAINS = [
         {name: "Kilimanjaro", height: 5895, country: "Tanzania"},
@@ -28,16 +28,16 @@
     ];
 
     function rowHeights(rows) {
-        return rows.map(function(row) {
-            return row.reduce(function(max, cell) {
+        return rows.map(function (row) {
+            return row.reduce(function (max, cell) {
                 return Math.max(max, cell.minHeight());
             }, 0);
         });
     }
 
     function colWidths(rows) {
-        return rows[0].map(function(_, i) {
-            return rows.reduce(function(max, row) {
+        return rows[0].map(function (_, i) {
+            return rows.reduce(function (max, row) {
                 return Math.max(max, row[i].minWidth());
             }, 0);
         });
@@ -48,16 +48,16 @@
         var widths = colWidths(rows);
 
         function drawLine(blocks, lineNo) {
-            return blocks.map(function(block) {
+            return blocks.map(function (block) {
                 return block[lineNo];
             }).join(" ");
         }
 
         function drawRow(row, rowNum) {
-            var blocks = row.map(function(cell, colNum) {
+            var blocks = row.map(function (cell, colNum) {
                 return cell.draw(widths[colNum], heights[rowNum]);
             });
-            return blocks[0].map(function(_, lineNo) {
+            return blocks[0].map(function (_, lineNo) {
                 return drawLine(blocks, lineNo);
             }).join("\n");
         }
@@ -66,26 +66,29 @@
     }
 
     function repeat(string, times) {
-        var result = "";
-        for (var i = 0; i < times; i++)
+        var result = "", i;
+        for (i = 0; i < times; i++) {
             result += string;
+        }
         return result;
     }
 
     function TextCell(text) {
         this.text = text.split("\n");
     }
-    TextCell.prototype.minWidth = function() {
-        return this.text.reduce(function(width, line) {
+
+    TextCell.prototype.minWidth = function () {
+        return this.text.reduce(function (width, line) {
             return Math.max(width, line.length);
         }, 0);
     };
-    TextCell.prototype.minHeight = function() {
+    TextCell.prototype.minHeight = function () {
         return this.text.length;
-    }
-    TextCell.prototype.draw = function(width, height) {
-        var result = [];
-        for (var i = 0; i < height; i++) {
+    };
+
+    TextCell.prototype.draw = function (width, height) {
+        var i, result = [];
+        for (i = 0; i < height; i++) {
             var line = this.text[i] || "";
             result.push(line + repeat(" ", width - line.length));
         }
@@ -93,24 +96,25 @@
     };
     function UnderlinedCell(inner) {
         this.inner = inner;
-    };
-    UnderlinedCell.prototype.minWidth = function() {
+    }
+
+    UnderlinedCell.prototype.minWidth = function () {
         return this.inner.minWidth();
     };
-    UnderlinedCell.prototype.minHeight = function() {
+    UnderlinedCell.prototype.minHeight = function () {
         return this.inner.minHeight() + 1;
     };
-    UnderlinedCell.prototype.draw = function(width, height) {
+    UnderlinedCell.prototype.draw = function (width, height) {
         return this.inner.draw(width, height - 1)
             .concat([repeat("-", width)]);
     };
     function dataTable(data) {
         var keys = Object.keys(data[0]);
-        var headers = keys.map(function(name) {
+        var headers = keys.map(function (name) {
             return new UnderlinedCell(new TextCell(name));
         });
-        var body = data.map(function(row) {
-            return keys.map(function(name) {
+        var body = data.map(function (row) {
+            return keys.map(function (name) {
                 return new TextCell(String(row[name]));
             });
         });
